9bee20c3a1bbcc20c6a93b180b5d3aa0111e0958
Hoimn <737753@student.dusd.net>
2023-11-16 00:46:39 -0800

    I finished the animation for the Robots and Ruby
    I finished the animations for the robots and Ruby

--- RubysAdventureGonzalez/Assets/Scripts/EnemyController.cs
+++ RubysAdventureGonzalez/Assets/Scripts/EnemyController.cs
@@ -11,12 +11,17 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
-    // Start is called before the first frame update
+
+    Animator animator;
+
+    //Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
+
     void Update()
     {
         timer -= Time.deltaTime;
@@ -35,19 +40,25 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+
         }
         else
         {
-            position.x = position.x + Time.deltaTime *speed * direction;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
     }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
 
-        if (player != null)
+        if (player != null) 
         {
             player.ChangeHealth(-1);
         }
